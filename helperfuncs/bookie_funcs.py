d5c98f3250e148cdba8f575cfccaf007d42e71b0
KTO23 <kyle.okura@yahoo.com>
2025-02-28 04:18:27 -0700

    tired

--- helperfuncs/bookie_funcs.py
+++ helperfuncs/bookie_funcs.py
@@ -1,8 +1,23 @@
 from ..tools.bet_history import *
 
 
-#display_ev_bookie_table()
+display_ev_bookie_table()
+
+print()
+
+'''
+create_bookie_table()
+print()
+add_bookmaker('draftkings', 100, 0, 0, 137.84)
+add_bookmaker('fanduel', 100, 0, 45, 98)
+add_bookmaker('betmgm', 150, 0, 25, 112)
+add_bookmaker('betrivers', 100, 0, 125, 42.50)
+add_bookmaker('ballybet', 60, 0, 30, 94.25)
+add_bookmaker('espnbet', 10, 0, 10, 100.02)
+add_bookmaker('fanatics', 0, 0, 0, 0)
+'''
 
 
 
-#display_bookie_table()
+transfer_bets()
+display_ev_bookie_table()
--- tools/bet_history.py
+++ tools/bet_history.py
@@ -86,6 +86,69 @@ def create_tables():
     conn.commit()
     conn.close()
 
+def create_bookie_table():
+    db_path = get_path()
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute('''DROP TABLE bookies''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS bookies(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   bookmaker VARCHAR(20),
+                   deposit_total float,
+                   withdrawl_total float,
+                   total_bankroll float,
+                   currently_wagered float,
+                   wagerable float,
+                   current_net float
+                   );''')
+    
+    
+    conn.commit()
+    conn.close()
+
+
+def transfer_bets():
+    db_path = get_path()
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT * FROM bookies''')
+    bookies_data = cursor.fetchall()
+
+    cursor.execute('''DROP TABLE bookies''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS bookies(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   bookmaker VARCHAR(20),
+                   deposit_total float,
+                   withdrawal_total float,
+                   total_bankroll float,
+                   currently_wagered float,
+                   wagerable float,
+                   current_net float,
+                   bets_placed int,
+                   bets_settled int,
+                   bets_won int,
+                   bets_lost int
+                   );''')
+    
+
+    for x in bookies_data:
+        name = x['bookmaker']
+        deposit = x['deposit_total']
+        withdrawn = x['withdrawl_total']
+        bankroll = x['total_bankroll']
+        wagered = x['currently_wagered']
+        wagerable = x['wagerable']
+        net = x['current_net']
+        cursor.execute('''INSERT INTO bookies (bookmaker, deposit_total, withdrawal_total, total_bankroll, currently_wagered, wagerable, current_net, bets_placed, bets_settled, bets_won, bets_lost)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?)''', (name, deposit, withdrawn, bankroll, wagered, wagerable, net, 0, 0, 0, 0))
+        
+    conn.commit()
+    conn.close()
+
+    
 
 def get_bookies_table():
     print("Open db in get_bookies_table")
